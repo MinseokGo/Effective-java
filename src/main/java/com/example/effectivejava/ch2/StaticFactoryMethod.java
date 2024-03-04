8ecf67889c8260b059fb8f98852c85ccf86a86cf
MinseokGo <rhalstjr1999@naver.com>
2024-03-04 16:23:51 +0900

    feat: static factory method

---
+++ src/main/java/com/example/effectivejava/ch2/StaticFactoryMethod.java
@@ -0,0 +1,15 @@
+package com.example.effectivejava.ch2;
+
+/*
+* item1: 생성자 대신 정적 팩터리 메서드를 고려하라
+*/
+public class StaticFactoryMethod {
+    // 기본 생성자로 인스턴스 할당 받는 법
+    public StaticFactoryMethod() {
+    }
+
+    // 정적 팩터리 메서드로 인스턴스 할당 받는 법
+    public StaticFactoryMethod getInstance() {
+        return new StaticFactoryMethod();
+    }
+}
