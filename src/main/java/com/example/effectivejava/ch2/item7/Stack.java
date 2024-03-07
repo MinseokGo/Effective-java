93eab1431af32e2c8389b6cf5b2c7670f2af866a
MinseokGo <rhalstjr1999@naver.com>
2024-03-07 20:26:41 +0900

    feat: free reference

---
+++ src/main/java/com/example/effectivejava/ch2/item7/Stack.java
@@ -0,0 +1,35 @@
+package com.example.effectivejava.ch2.item7;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+public class Stack {
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    private Object[] elements;
+    private int size = 0;
+
+    public Stack() {
+        elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public void push(final Object element) {
+        ensureCapacity();
+        elements[size++] = element;
+    }
+
+    private void ensureCapacity() {
+        if (elements.length == size) {
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+        }
+    }
+
+    public Object pop() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        final Object element = elements[--size];
+        elements[size] = null;  // 다 쓴 참조 해제
+        return element;
+    }
+}
