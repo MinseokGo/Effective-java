a1e4fa5eaac29ad7a14999c9cc9d1aa3e3d63518
MinseokGo <rhalstjr1999@naver.com>
2024-03-27 16:15:53 +0900

    refactor: deep copy by for

--- src/main/java/com/example/effectivejava/ch3/item13/HashTable.java
+++ src/main/java/com/example/effectivejava/ch3/item13/HashTable.java
@@ -14,10 +14,13 @@ public class HashTable implements Cloneable {
             this.next = next;
         }
 
-        // 이 엔트리가 가리키는 연결 리스트를 재귀적으로 복사
+        // 스택 오버플로우를 고려하여 재귀 대신 반복문을 통해 깊은 복사
         Entry deepCopy() {
-            return new Entry(key, value,
-                    next == null ? null : next.deepCopy());
+            Entry result = new Entry(key, value, next);
+            for (Entry entry = result; entry.next != null; entry = entry.next) {
+                entry.next = new Entry(entry.next.key, entry.next.value, entry.next.next);
+            }
+            return result;
         }
     }
 
@@ -27,7 +30,7 @@ public class HashTable implements Cloneable {
             HashTable result = (HashTable) super.clone();
             result.buckets = new Entry[buckets.length];
             for (int i = 0; i < buckets.length; i++) {
-                if (buckets[i] == null) {
+                if (buckets[i] != null) {
                     result.buckets[i] = buckets[i];
                 }
             }
